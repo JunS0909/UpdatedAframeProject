90e46fc5d66f233c5e6f5dd0e952e56e077aaf92
JunS0909 <juns3@nycstudents.net>
2025-02-14 13:57:35 +0000

    h

--- birds.js
+++ birds.js
@@ -3,10 +3,10 @@ class Bird{
      x = this.x;
      y = this.y;
      z = this.z;   
-
+    h = 40;
      this.obj = document.createElement("a-gltf-model");
         this.obj.setAttribute("src","#birds");
-        this.obj.setAttribute("position",{x:this.x,y:40,z:this.z});
+        this.obj.setAttribute("position",{x:this.x,y:this.y + h,z:this.z});
         this.obj.setAttribute("scale",{x:2,y:2,z:2});
         this.obj.setAttribute("animation-mixer","");
         scene.append(this.obj);

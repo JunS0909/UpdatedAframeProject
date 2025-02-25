a9254ad4151caa790abdd6b440a72f2ab5061ce1
JunS0909 <juns3@nycstudents.net>
2025-02-25 02:35:42 +0000

    a

--- script.js
+++ script.js
@@ -36,12 +36,7 @@ window.onload = function(){
 	loop();
 }
 function loop(){
-	//console.log(mainCamera.object3D.position.x);
-	//console.log(mainCamera.object3D.position.z);
-	//console.log(c.camera.object3D.position.x);
-	//console.log(c.camera.object3D.position.z);
-	//console.log(lightsources.object3D.rotation.y);
-	birds.birdsFly;
+	birds.birdsFly();
 	lightsources.object3D.rotation.z += 0.002;
 	window.requestAnimationFrame(loop);
 

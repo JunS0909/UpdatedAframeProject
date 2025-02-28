507b19298efe8a39917e6b69d795f614eae5f795
JunS0909 <juns3@nycstudents.net>
2025-02-28 09:56:45 +0000

    a

--- trees.js
+++ trees.js
@@ -1,5 +1,5 @@
 
-class Tree{
+class Ball{
 	constructor(x,y,z){
 		this.x = x;
 		this.z = z;
@@ -12,7 +12,7 @@ class Tree{
 		this.obj.setAttribute("scale",{x:40,y:60,z:40});
 		scene.append(this.obj);
 
-	this.obj.setAttribute("position",{x:this.x,y:-6.5,z:this.z});
+	this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
       scene.append(this.obj);
       }
 	  

34d8c9b33aad13df471764c64f55f6a86b9dc36f
JunS0909 <juns3@nycstudents.net>
2025-02-28 10:24:42 +0000

    a

--- script.js
+++ script.js
@@ -7,7 +7,26 @@ window.onload = function(){
   moon = document.getElementById("moon");
   mainCamera = document.getElementById("mainCamera");
   tp = document.getElementById("teleport");
+  school = document.getElementById("school");
   nCamera = document.getElementById("nCamera");
+  randoma = document.getElementById("randoma");
+  randomb = document.getElementById("randomb");
+  randomc = document.getElementById("randomc");
+  randomd = document.getElementById("randomd");
+  randome = document.getElementById("randome");
+  randomf = document.getElementById("randomf");
+  randomg = document.getElementById("randomg");
+  randomh = document.getElementById("randomh");
+  randomi = document.getElementById("randomi");
+  randomj = document.getElementById("randomj");
+  randomk = document.getElementById("randomk");
+  randoml = document.getElementById("randoml");
+  randomm = document.getElementById("randomm");
+  randomn = document.getElementById("randomn");
+  randomo = document.getElementById("randomo");
+  randomp = document.getElementById("randomp");
+  randomq = document.getElementById("randomq");
+  randomr = document.getElementById("randomr");
 
   for (let i = 0; i < 30; i++) {
 	let x = Math.random() * (200 - (-200)) + (-200);  
@@ -32,6 +51,10 @@ for (let i = 0; i<10;i++){
 		c.camera.setAttribute("active",false);
 	})
 	
+	randoma.addEventlistener("click",function(){
+		school.setAttribute("kinema-body","");
+	})
+
 	window.addEventListener("keydown",function(e){
 		
 		 if(e.key == "e"){
@@ -58,14 +81,27 @@ function loop(){
 	for(let Bird of birds){
 		Bird.fly();
 	}
+
 	let sky = document.querySelector("a-sky");
 	if (sun.object3D.rotation.y > Math.PI / 2) {
 		sky.setAttribute("color", "lightblue"); 
 	  } else {
 		sky.setAttribute("color", "darkblue"); 
 	  }
-
+	if(collision(c,person)<1){
+		console.log(collide);
+	}
 
 	  window.requestAnimationFrame(loop);
 
+}
+function collision(obj1, obj2){
+	let x1 = obj1.object3D.position.x;
+	let z1 = obj1.object3D.position.z;
+	let x2 = obj2.object3D.position.x;
+	let z2 = obj2.object3D.position.z;
+
+	let distance=Math.sqrt((x2-x1)**2 + (z2-z1)**2);
+
+	return distance;
 }
